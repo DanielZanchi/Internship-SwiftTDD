327e0a9fc5a35447acd9448f41b9465adef20541
Daniel Zanchi <danny.zanchi@gmail.com>
2019-03-20 11:12:05 +0100

    separate file

---
+++ FuelTracker/Controller/FuelRegisterTableView.swift
@@ -0,0 +1,62 @@
+//
+//  FuelRegisterTableView.swift
+//  FuelTracker
+//
+//  Created by Daniel Zanchi on 20/03/2019.
+//  Copyright © 2019 Daniel Zanchi. All rights reserved.
+//
+
+import UIKit
+
+extension FuelRegisterViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        var rows = 0
+        if let fuels = fuelsManager.getFuels() {
+            rows = fuels.count
+        }
+        return rows
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {        
+        let cell = tableView.dequeueReusableCell(withIdentifier: FuelTableViewCell.identifier) as! FuelTableViewCell
+        
+        if let fuel = fuelsManager.getFuels()?[indexPath.row] {
+            var distance: Int = 0
+            var consumption: Double = 0.00
+            
+            if let fuelsCount = fuelsManager.getFuels()?.count {
+                if (indexPath.row + 1) < fuelsCount {
+                    if let oldFuel = fuelsManager.getFuels()?[indexPath.row + 1] {
+                        distance = FuelsHelper.calculateDistance(from: fuel, to: oldFuel)
+                        consumption = FuelsHelper.calculateConsumption(distance: distance, fuelQuantity: fuel.quantity)
+                    }
+                }
+            }
+            
+            let fuelView = FuelViewModel(fuel: fuel)
+            
+            cell.setup(model: fuelView, distance: distance, consumption: consumption)
+            
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { 
+        if let EditViewController = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "AddFuelVC") as? AddFuelViewController {
+            
+            if let fuelToEdit = fuelsManager.getFuels()?[indexPath.row] {
+                EditViewController.fuelToEdit = fuelToEdit
+                EditViewController.isEditingFuel = true
+                
+                self.navigationController?.pushViewController(EditViewController, animated: true)
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+    }
+    
+    
+}
---
+++ FuelTracker/Model/FuelManager.swift
@@ -0,0 +1,121 @@
+//
+//  FuelManager.swift
+//  FuelTracker
+//
+//  Created by Daniel Zanchi on 18/03/2019.
+//  Copyright © 2019 Daniel Zanchi. All rights reserved.
+//
+
+import Foundation
+import SQLite
+
+class FuelsManager {
+    
+    private var database: Connection?
+    
+    private let fuelsTable = Table("fuels")
+    
+    let id = Expression<Int64>("id")
+    let date = Expression<Date>("date")
+    let mileage = Expression<Int>("mileage")
+    let quantity = Expression<Double>("quantity")
+    let pricePerUnit = Expression<Double>("pricePerUnit")
+    
+    init(database: Connection) {
+        self.database = database
+        
+        createTable()
+    }
+
+    @discardableResult func createTable() -> Bool {
+        let create = self.fuelsTable.create { (table) in
+            table.column(id, primaryKey: true)
+            table.column(date)
+            table.column(mileage)
+            table.column(quantity)
+            table.column(pricePerUnit)
+        }
+        
+        do {
+            try database?.run(create)
+            return true
+        } catch {
+            print("unable to create table")
+            print(error)
+            return false
+        }
+    }
+    
+    func addFuel(dateOfFuel: Date, mileageOnSave: Int, quantityOfFuel: Double, pricePerUnitOfFuel: Double) -> Int64 {
+        let insertFuel = fuelsTable.insert(date <- dateOfFuel, mileage <- mileageOnSave, quantity <- quantityOfFuel, pricePerUnit <- pricePerUnitOfFuel)
+        do {
+            let id = try database!.run(insertFuel)
+            return id
+        } catch {
+            print(error)
+            return -1
+        }
+    }
+    
+    func getFuels() -> [Fuel]? {
+        var fuels = [Fuel]()
+        
+        do {
+            if let fuelsList = try database?.prepare(fuelsTable.order(date.desc)) {
+                for fuel in fuelsList {
+                    fuels.append(Fuel(id: fuel[id], date: fuel[date], mileage: fuel[mileage], quantity: fuel[quantity], pricePerUnit: fuel[pricePerUnit]))
+                }
+            } else {
+                print("error")
+            }
+        } catch {
+            print("error while reading fuels")
+            print(error)
+            return nil
+        }
+        
+        return fuels
+    }
+    
+    func deleteFuel(fID: Int64) -> Bool {
+        do {
+            let fuel = fuelsTable.filter(id == fID)
+            try database?.run(fuel.delete())
+            return true
+        } catch {
+            print(error)
+            return false
+        }
+    }
+    
+    func updateFuel(fID: Int64, newFuel: Fuel) -> Bool {
+        let fuel = fuelsTable.filter(id == fID)
+        do {
+            let update = fuel.update([
+                date <- newFuel.date, 
+                mileage <- newFuel.mileage, 
+                quantity <- newFuel.quantity, 
+                pricePerUnit <- newFuel.pricePerUnit
+                ])
+            
+            if try database!.run(update) > 0 {
+                return true
+            }
+        } catch {
+            print(error)
+        }
+        
+        return false
+    }
+    
+    @discardableResult func dropTable() -> Bool {
+        do {
+            try database?.run(fuelsTable.drop())
+            return true
+        } catch {
+            print(error)
+            print("wasn't able to drop table")
+            return false
+        }
+    }
+}
